ff012bb7be407b1659907b1371c8422ee49e3f04
Hernani Samuel Diniz <86131133+HernaniSamuel@users.noreply.github.com>
2023-09-09 22:01:18 -0300

    aviso de continuacao

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,6 +4,7 @@ export default function Banner(){
     return(
         <div className={styles.banner_container}>
             <h1 className={styles.software}>Software</h1>
+            <p>Continua em 13/09/2023</p>
         </div>
     )
 }
