e602fa43c90c266a0881dd7272d9581d296565ce
Hernani <hernanisamuel0@gmail.com>
2024-01-17 20:08:21 -0300

    mudança de projeto e atualização de currículo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,10 @@
 import styles from '/home/hernani/PycharmProjects/meu_portfolio/src/components/Projects.module.css'
 
 import ProjectView from "./ProjectView.jsx"
-
+import Live from "./Live.jsx"
 
 import TodoPhone from '../images/Todo_phone.png'
 import TodoTablet from '../images/Todo_tablet.png'
-
-import TaroPhone from '../images/Taro_phone.png'
-import TaroTablet from '../images/Taro_tablet.png'
 
 import WorldPhone from '../images/World_phone.png'
 import WorldTablet from '../images/World_tablet.png'
@@ -15,31 +12,33 @@ import WorldTablet from '../images/World_tablet.png'
 import MarketTablet from '../images/Market_tablet.png'
 import MarketPhone from '../images/Market_phone.png'
 
+import LocadoraPhone from '../images/locadora_phone.png'
+import LocadoraTablet from '../images/locadora_tablet.png'
+
 
 export default function Projects(){
     return(
         <section className={styles.projects_container} id='projects'>
             <h1 className={styles.projects_title}>Meus Projetos</h1>
             <div className={styles.flex1}>
+            <ProjectView nome="Locadora de Veículos"
+                urlCell={LocadoraPhone}
+                urlTablet={LocadoraTablet}
+                urlProject='https://locadora-de-veiculos.onrender.com/'
+                stackIcons={[<Live/>, <span>(Obs: demora cerca de 1 minuto para abrir)</span>]}/>
+
             <ProjectView nome="Flask Market"
                 urlCell={MarketPhone}
                 urlTablet={MarketTablet}
                 urlProject='https://flask-market-s9d9.onrender.com/'
                 stackIcons="Flask, HTML, Bootstrap (obs: demora cerca de 1 minuto para abrir)"/>
-
-                <ProjectView nome="Lista de Tarefas"
+            </div>
+            <div className={styles.flex2}>
+            <ProjectView nome="Lista de Tarefas"
                 urlCell={TodoPhone}
                 urlTablet={TodoTablet}
                 urlProject='https://lista-de-tarefas-yi7q.onrender.com/login?next=%2F'
                 stackIcons="Flask, HTML, Bootstrap (obs: demora cerca de 1 minuto para abrir)"/>
-
-            </div>
-            <div className={styles.flex2}>
-            <ProjectView nome="Tarô Oficial"
-                urlCell={TaroPhone}
-                urlTablet={TaroTablet}
-                urlProject='https://hernanisamuel.github.io/Taro-Oficial/TarotWebsite/index'
-                stackIcons="HTML e CSS"/>
 
                 <ProjectView nome="World Graphics"
                 urlCell={WorldPhone}
